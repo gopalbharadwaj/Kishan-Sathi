5cd257f2ecbe268505a0628a01f15d808ad0c297
Gopal Bhardwaj <gopalbhardwaj2627@gmail.com>
2026-06-06 12:45:00 +0530

    Fix production socket

--- kishan-sathi-backend/src/socket/socket.js
+++ kishan-sathi-backend/src/socket/socket.js
@@ -8,7 +8,10 @@ const initSocket = (server) => {
 
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: [
+        "http://localhost:5173",
+        "https://kishan-sathi.onrender.com"
+      ],
       methods: ["GET", "POST"]
     }
   });
